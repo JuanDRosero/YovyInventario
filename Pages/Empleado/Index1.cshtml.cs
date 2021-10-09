5a6ed9833d189b7ed2002d3d306ab0b78a772e7b
Juan David <jdrosero59@gmail.com>
2021-10-09 09:37:30 -0500

    Correcion validación Formularios

--- Pages/Empleado/Index1.cshtml.cs
+++ Pages/Empleado/Index1.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -18,8 +19,11 @@ namespace YovyInventario.Pages.Empleado
         public IEnumerable<Ventum> ventas { get; set; }
         [BindProperty]
         public IEnumerable<Producto> productos { get; set; }
+        [DataType(DataType.Date)][BindProperty]
+        public DateTime fechaInv { get; set; }
+        [DataType(DataType.Date)]
         [BindProperty]
-        public Usuario usuario { get; set; }
+        public DateTime fechaVentas { get; set; }
         public IndexModel(YovyDBContext contex)
         {
             _contex = contex;
@@ -28,10 +32,11 @@ namespace YovyInventario.Pages.Empleado
         }
         public ActionResult OnGet()
         {
+           
             if (HttpContext.Session.GetInt32("_id") != -1)
             {
                 int id = (int)HttpContext.Session.GetInt32("_id");
-                inventario = _contex.Inventarios.ToList().Where(e => e.Fkusuario == id);
+                //inventario = _contex.Inventarios.ToList().Where(e => e.Fkusuario == id);
                 ventas = _contex.Venta.ToList().Where(e => e.Fkvendedor == id);
                 return Page();
             }
@@ -41,6 +46,13 @@ namespace YovyInventario.Pages.Empleado
             }
             
             
+        }
+        public ActionResult OnPost()
+        {
+            int id = (int)HttpContext.Session.GetInt32("_id");
+            if
+            inventario = _contex.Inventarios.ToList().Where(e => e.Fkusuario == id).Where(e=>e.Fecha==fechaInv);
+            return Page();
         }
     }
 }
